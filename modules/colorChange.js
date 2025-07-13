d8f3d9f0965e38cdbf03c31c04ca510e885dec5b
Balavignesh rajasekar <vigneshvickybsc1999@gmail.com>
2025-07-13 16:27:54 +0530

    New: UI created

--- modules/colorChange.js
+++
@@ -1,5 +0,0 @@
-export function changeColor() {
-  const elements = document.getElementById("h1");
-
-  elements.style.backgroundColor = "yellow";
-}
--- modules/main.js
+++
@@ -1,4 +0,0 @@
-import { changeColor } from "./colorChange.js";
-
-const h1 = document.getElementById("h1");
-h1.addEventListener("click", changeColor);
